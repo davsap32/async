5f6b37d220206a0091e63b15fbaa83a78c7fda01
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-09-02 19:09:05 +0200

    trying to read object outside of asynk fn, not working yet

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const contentLarge = document.querySelector(".content-large");
 const contentClock = document.querySelector(".clock");
 const contentSmall1 = document.querySelector(".content-small-1");
 
-async function fetchUsers(data) {
+async function fetchUsers() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
    data = await res.json();
-  console.log(data[1].address.city);
 	for (const key in data[1].address) {
-    contentLarge.insertAdjacentHTML("beforeend", `${key}: ${data[1].address[key]}`);
+		let txt = data[1].address[key]
+    contentLarge.insertAdjacentHTML("beforeend", `<p>${key}: ${txt}<p>`);
 	}
-	contentLarge.textContent = data[1].address.city;
+	return data;
 }
-fetchUsers();
-
+const person = fetchUsers().then();
+for (const key in person[1].address) {
+	let txt = data[1].address[key];
+	console.log(txt);
+}
