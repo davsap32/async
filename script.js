ecce80403fdb5a7e8c1c541955be6a4e2f2de1f8
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-09-02 18:43:57 +0200

    beginning to unpack the json object

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-async function fetchUsers() {
+const contentLarge = document.querySelector(".content-large");
+const contentClock = document.querySelector(".clock");
+const contentSmall1 = document.querySelector(".content-small-1");
+
+async function fetchUsers(data) {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-  console.log(data);
+   data = await res.json();
+  console.log(data[1].address.city);
+	for (const key in data[1].address) {
+    contentLarge.insertAdjacentHTML("beforeend", `${key}: ${data[1].address[key]}`);
+	}
+	contentLarge.textContent = data[1].address.city;
 }
 fetchUsers();
+
