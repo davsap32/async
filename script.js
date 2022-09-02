f518c944f8f3b757fe966640257524eff280d9ae
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-09-02 22:48:26 +0200

    now able to retrieve returned data outside of the async fn

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+'use strict';
 const contentLarge = document.querySelector(".content-large");
 const contentClock = document.querySelector(".clock");
 const contentSmall1 = document.querySelector(".content-small-1");
 
 async function fetchUsers() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  data = await res.json();
+  const data = await res.json();
 	for (const key in data[1].address) {
 		let txt = data[1].address[key];
     contentLarge.insertAdjacentHTML("beforeend", `<p>${key}: ${txt}<p>`);
 	}
+	return data;
 }
-fetchUsers();
-
+fetchUsers().then(
+	function(value){
+	for (const key in value[1].address) {
+		let txt = value[1].address[key];
+		console.log(`${key}: ${txt}`);
+	}
+	
+});
+/*
+async function myFunction() {
+  return "Hello";
+}
+myFunction().then(
+  function(value) {myDisplayer(value);} 
+	);
+	
+*/
