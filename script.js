a8f2501aff179679d109adcf70ade15ee1d3c716
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-09-02 19:17:28 +0200

    new try to read returned data outside of the async fn, not succeeding yet

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ const contentSmall1 = document.querySelector(".content-small-1");
 
 async function fetchUsers() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-   data = await res.json();
+  data = await res.json();
 	for (const key in data[1].address) {
-		let txt = data[1].address[key]
+		let txt = data[1].address[key];
     contentLarge.insertAdjacentHTML("beforeend", `<p>${key}: ${txt}<p>`);
 	}
-	return data;
 }
-const person = fetchUsers().then();
-for (const key in person[1].address) {
-	let txt = data[1].address[key];
-	console.log(txt);
-}
+fetchUsers();
+
