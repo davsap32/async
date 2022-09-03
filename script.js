6a76e00509e8145663dd3390684b5caa288137ac
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-09-03 08:49:56 +0200

    added recursive fn to flatten nested object

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-'use strict';
+"use strict";
 const contentLarge = document.querySelector(".content-large");
 const contentClock = document.querySelector(".clock");
 const contentSmall1 = document.querySelector(".content-small-1");
 let date;
 let dateTimeInMs;
+const flat = (value, out) => {
+  Object.keys(value).forEach((key) => {
+    if (typeof value[key] == "object") {
+      out = flat(value[key], out); //recursively call for nesteds
+    } else {
+      out[key] = value[key]; //direct assign for values
+    }
+    console.log(out);
+  });
+  return out;
+};
+const timeTxt = function (txt) {
+  date = new Date();
+  dateTimeInMs = Math.floor(date.getTime());
+  console.log(`Time in ms ${txt}: ${dateTimeInMs}`);
+};
 async function fetchUsers() {
-	date = new Date();
+  //date = new Date();
 
-	dateTimeInMs = Math.floor(date.getTime());
-	console.log('time in ms before wait call', dateTimeInMs);
-
+  //dateTimeInMs = Math.floor(date.getTime());
+  //console.log("time in ms before wait call", dateTimeInMs);
+  timeTxt("before wait call");
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
-	for (const key in data[1].address) {
-		let txt = data[1].address[key];
+  for (const key in data[1].address) {
+    let txt = data[1].address[key];
     contentLarge.insertAdjacentHTML("beforeend", `<p>${key}: ${txt}<p>`);
-	}
-	date = new Date();
-	dateTimeInMs = Math.floor(date.getTime());
-	//console.log('time in ms at end of fetch fn', dateTimeInMS);
-	return data;
+  }
+  timeTxt("at end of fetch fn");
+  return data;
 }
-fetchUsers().then(
-	function(value){
-	for (const key in value[1].address) {
-		let txt = value[1].address[key];
-		console.log(`${key}: ${txt}`);
-	}
-	
+fetchUsers().then(function (value) {
+  for (const key in value[1].address) {
+    let txt = value[1].address[key];
+    console.log(`${key}: ${txt}`);
+  }
+  const out = {};
+  const flattened = flat(value, out);
 });
-date = new Date();
-dateTimeInMs = Math.floor(date.getTime() / 1000);
-console.log('time in ms at end of script', dateTimeInMs);
+timeTxt("at end of script");
 /*
 async function myFunction() {
   return "Hello";
